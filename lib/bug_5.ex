cc78a054dd5897dabf3bcc08ff6073339e11f34b
GeoffreyPS <geoffreyps@gmail.com>
2016-12-07 18:53:38 -0600

    comment out modules for demo

--- lib/bug_5.ex
+++ lib/bug_5.ex
@@ -15,22 +15,22 @@ defmodule Cashy.Bug5 do
 end
 
 
-defmodule Cashy.Bug5 do
-  #type specifications can help 
+# defmodule Cashy.Bug5 do
+#   #type specifications can help 
 
-  # @type currency() :: :sgd | :usd
-  # @spec convert(currency, currency, number) :: number
+#   @type currency() :: :sgd | :usd
+#   @spec convert(currency, currency, number) :: number
   
-  # def convert(:sgd, :usd, amount) do
-  #   amount * 0.70
-  # end
+#   def convert(:sgd, :usd, amount) do
+#     amount * 0.70
+#   end
 
-  # @spec amount({:value, number}) :: number
-  # def amount({:value, value}) do
-  #   value 
-  # end
+#   @spec amount({:value, number}) :: number
+#   def amount({:value, value}) do
+#     value 
+#   end
   
-  # def run do
-  #   convert(:sgd, :usd, amount({:value, :one_million_dollars}))
-  # end 
-end
+#   def run do
+#     convert(:sgd, :usd, amount({:value, :one_million_dollars}))
+#   end 
+# end
